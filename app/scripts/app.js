1199e90f7379c5129dd9d1ff620081e57ff02671
thorben_interaktiv <thorben@die-interaktiven.de>
2016-08-28 20:24:08 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name isbHa2App
+ * @description
+ * # isbHa2App
+ *
+ * Main module of the application.
+ */
+angular.module('isbHa2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name isbHa2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the isbHa2App
+ */
+angular.module('isbHa2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
